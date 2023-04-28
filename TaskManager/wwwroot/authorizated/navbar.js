8f321669317904844006345ed5ad6d509f658732
Vlad <vlad.fiodorov11@gmail.com>
2023-04-28 18:40:14 +0300

    Login page fix and changing main layout

--- TaskManager/wwwroot/authorizated/navbar.js
+++ TaskManager/wwwroot/authorizated/navbar.js
@@ -1,19 +1,30 @@
 ﻿const dropdownBtn = document.getElementById("nav-btn");
 const dropdownList = document.getElementById("nav-dropdown");
 
+document.addEventListener('click', (event) => {
+    const isClickInside = dropdownList.contains(event.target);
+    const isClickInsideBtn = dropdownBtn.contains(event.target);
+    if (dropdownList.style.display == "block" && !isClickInside && !isClickInsideBtn) {
+        dropdownList.style.display = "none";
+    }
+});
+
 dropdownBtn.addEventListener("click", function () {
-    if (dropdownList.style.display === "none") {
+    if (dropdownList.style.display != "block") {
         dropdownList.style.display = "block";
     } else {
         dropdownList.style.display = "none";
     }
 });
 
-document.addEventListener('click', (event) => {
-    const isClickInside = dropdownList.contains(event.target);
-    const isClickInsideBtn = dropdownBtn.contains(event.target);
-    if (dropdownList.style.display == "block" && !isClickInside && !isClickInsideBtn) {
-        dropdownList.style.display = "none";
+dropdownBtn.addEventListener('click', function () {
+    // добавляем или удаляем класс "active" в зависимости от того, открыт ли выпадающий список
+    if (dropdownList.classList.contains('active')) {
+        dropdownList.classList.remove('active');
+        dropdownBtn.classList.remove('active');
+    } else {
+        dropdownList.classList.add('active');
+        dropdownBtn.classList.add('active');
     }
 });
 
